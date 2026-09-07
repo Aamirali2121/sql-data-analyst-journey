e24bef8d4a02d3e3119c5281f13277b66791f3f8
Aamirali2121 <108357776+Aamirali2121@users.noreply.github.com>
2026-09-07 14:30:43 +0530

    Update 01_select_where.sql

--- database/practice/01_select_where.sql
+++ database/practice/01_select_where.sql
@@ -6,3 +6,34 @@ SELECT
     city
 FROM customers
 WHERE city = 'Mumbai';
+
+-- Requirement 2 — ORDER BY
+-- Business Goal
+
+-- The product team wants to see all products ranked from the most expensive to the least expensive.
+
+select 
+	product_id,
+    product_name,
+    unit_cost
+from products
+order by
+	unit_cost desc;
+
+-- Requirement 3 — AND
+-- Business Goal
+
+-- The CRM team wants customers who:
+
+-- live in Delhi
+
+-- and signed up after 2025-04-01
+
+select 
+	customer_name,
+    city,
+    signup_date
+from customers
+where signup_date > '2025-04-01'
+and
+	city = 'Delhi';
