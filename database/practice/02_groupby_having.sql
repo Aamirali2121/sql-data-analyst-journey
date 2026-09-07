c3b6a014e9026e46a41b55f2a1e86603bd3fa965
Aamirali2121 <108357776+Aamirali2121@users.noreply.github.com>
2026-09-07 15:11:27 +0530

    Update 02_groupby_having.sql

--- database/practice/02_groupby_having.sql
+++ database/practice/02_groupby_having.sql
@@ -6,3 +6,25 @@ SELECT
     COUNT(*) AS customer_count
 FROM customers
 GROUP BY city;
+
+
+-- Requirement 5 — GROUP BY + AVG
+-- Business Goal
+-- The merchandising team wants to know the average product cost for each category.
+
+select 
+	category,
+    round(avg(unit_cost),2) as avg_unit_cost
+from products
+group by category;
+
+-- Requirement 6 — GROUP BY + HAVING
+-- Business Goal
+-- The merchandising director only wants to see categories whose average product cost exceeds ₹10,000.
+
+select
+	category,
+    round(avg(unit_cost),2) as avg_unit_cost
+from products 
+group by category
+having round(avg(unit_cost),2)>10000;
